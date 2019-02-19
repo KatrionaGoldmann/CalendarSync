7c8686595448936fa4fcd3d0a78e2ebcd5ebbfd2
PhD <k.goldmann@qmul.ac.uk>
2019-02-19 15:56:24 +0000

    tidy script

--- WHRI_JC_Sync.py
+++ WHRI_JC_Sync.py
@@ -3,13 +3,14 @@ import pandas as pd
 import numpy as np
 from DF_to_ICS import df_to_ics
 
-data_safe = pd.read_csv('https://docs.google.com/spreadsheets/d/1GpVOQ7-AOOjBDWdbif6VaKv4LUEQT2bhpAnQc7X6FF4/gviz/tq?tqx=out:csv&sheet=Sheet1')
+text_file = open("schedule_path.txt", "r")
+file = text_file.read()
+data_safe = pd.read_csv(file)
 
 data = data_safe
 data['Subject'] = data['Presenter'] + " presenting at journal club"
 
 data['Start Date'] = data['Date']
-print(data['Start Date'] )
 data['End Date'] = data['Date']
 data['Start Time'] = data['Start Time'].astype(str).str.split().str[1]
 data['Start Time'] = data['Start Time'].str.split(":").str[0] + ':' + data['Start Time'].str.split(":").str[1]
@@ -26,6 +27,5 @@ data = data[["Subject", "Start Date", "Start Time", "End Date", "End Time", "All
 
 
 data = data.dropna(axis=0, subset=['Subject'])
-print(data.head())
 
 df_to_ics(Filename="JC_schedule.ics", Hour=24, DF=data, Reminder=96)
